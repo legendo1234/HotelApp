73a10b0388e367724f071d5a6504c6c79fe2326a
Matej Weber <matej.weber@gmail.com>
2024-03-21 11:37:02 +0100

    Add project files.

---
+++ HotelAppDB/dbo/Tables/Rooms.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Rooms]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [RoomNumber] NVARCHAR(10) NOT NULL, 
+    [RoomTypeId] INT NOT NULL, 
+    CONSTRAINT [FK_Rooms_RoomTypes] FOREIGN KEY (RoomTypeId) REFERENCES RoomTypes(Id)
+    
+)
