123b0e92f9cb4082f16c94ed5c9de8a5329cceb3
Matej Weber <matej.weber@gmail.com>
2024-03-21 13:59:47 +0100

    Made stored procedure to list all room types ,  made room type model in library project, changed row type of start and end date to DATE

---
+++ HotelAppLibrary/Data/SqlData.cs
@@ -0,0 +1,28 @@
+﻿using HotelAppLibrary.Databases;
+using HotelAppLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelAppLibrary.Data
+{
+    public class SqlData
+    {
+        private readonly ISqlDataAccess _db;
+        private const string connectionStringName = "SqlDb";
+
+        public SqlData(ISqlDataAccess db)
+        {
+           _db = db;
+        }
+        public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
+        {
+            return _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetAvailableTypes",
+                                                 new { startDate, endDate },
+                                                 connectionStringName,
+                                                 true);
+        }
+    }
+}
