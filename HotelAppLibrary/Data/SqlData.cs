1350f864887df53e826817a9c4b6a6f3902f6a41
Matej Weber <matej.weber@gmail.com>
2024-03-21 18:45:04 +0100

    Added a bunch of new store procedures, made folder for the procedures so they dont sit in dbo, aded Models for Guests, rooms and the full model

--- HotelAppLibrary/Data/SqlData.cs
+++ HotelAppLibrary/Data/SqlData.cs
@@ -5,17 +5,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace HotelAppLibrary.Data
 {
-    public class SqlData
+    public class SqlData : IDatabaseData
     {
         private readonly ISqlDataAccess _db;
         private const string connectionStringName = "SqlDb";
 
         public SqlData(ISqlDataAccess db)
         {
-           _db = db;
+            _db = db;
         }
         public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
         {
@@ -23,6 +24,53 @@ namespace HotelAppLibrary.Data
                                                  new { startDate, endDate },
                                                  connectionStringName,
                                                  true);
+        }
+
+        public void BookGuest(string firstName,
+                         string lastName,
+                         DateTime startDate,
+                         DateTime endDate,
+                         int roomTypeId)
+        {
+            GuestModel guest = _db.LoadData<GuestModel, dynamic>("dbo.spGuests_Insert",
+                                                                 new { firstName, lastName },
+                                                                 connectionStringName,
+                                                                 true).First();
+
+            RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("select * from dbo.RoomTypes where Id= @Id",
+                                                                         new { Id = roomTypeId },
+                                                                         connectionStringName,
+                                                                         false).First();
+
+            TimeSpan timeStaying = endDate.Date.Subtract(startDate.Date);
+
+            List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.spRooms_GetAvailableRooms", new { startDate, endDate, roomTypeId }, connectionStringName, true);
+
+
+            _db.SaveData("spBookings_Insert",
+                new
+                {
+                    roomId = availableRooms.First().Id,
+                    guestId = guest.Id,
+                    startDate = startDate,
+                    endDate = endDate,
+                    totalCost = timeStaying.Days * roomType.Price
+                },
+                connectionStringName,
+                true);
+        }
+
+        public List<BookingFullModel> SearchBookings(string lastName)
+        {
+            return _db.LoadData<BookingFullModel, dynamic>("dbo.spBookings_Search",
+                                                    new { lastName, startDate = DateTime.Now.Date },
+                                                    connectionStringName,
+                                                    true);
+        }
+
+        public void CheckInGuest(int bookingId)
+        {
+            _db.SaveData("dbo.spBookings_CheckIn", new { id = bookingId }, connectionStringName, true);
         }
     }
 }
---
+++ HotelAppLibrary/Models/RoomModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelAppLibrary.Models
+{
+    public class RoomModel
+    {
+        public int Id { get; set; }
+        public string RoomNumber { get; set; }
+        public int RoomTypeId { get; set; }
+    }
+}
