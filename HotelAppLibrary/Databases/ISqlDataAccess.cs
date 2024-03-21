17e4cfc5344b674a10f1af98f48f92f6a3b2a616
Matej Weber <matej.weber@gmail.com>
2024-03-21 13:25:40 +0100

    Made data access class with save and load data ( connectiong string stuff), made interface of data access so I can reuse this when I want

---
+++ HotelAppLibrary/Databases/ISqlDataAccess.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HotelAppLibrary.Databases
+{
+    public interface ISqlDataAccess
+    {
+        List<T> LoadData<T, U>(string sqlStatement,
+                               U parameters,
+                               string connectionStringName,
+                              bool isStoredProcedure = false);
+        void SaveData<T>(string sqlStatement,
+                         T parameters,
+                         string connectionStringName,
+                         bool isStoredProcedure = false);
+    }
+}
